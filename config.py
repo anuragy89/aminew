31d1a35a48fefadf13568866407e326adb06707e
yuva neta <anuragyadavop89@gmail.com>
2026-07-12 12:03:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -104,22 +104,22 @@ confirmer = {}
 
 START_IMG_URL = getenv(
     "START_IMG_URL",
-    "https://te.legra.ph/file/5fd13f2cc0d03bce9f7f2.jpg https://te.legra.ph/file/c15d01b3e6b40ea141dc9.jpg https://te.legra.ph/file/5fd13f2cc0d03bce9f7f2.jpg",
+    "https://ibb.co/PGvmH533 https://ibb.co/PGvmH533 ",
 ).split()
     
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/87f680aead03443f291b0.jpg"
+    "PING_IMG_URL", "https://ibb.co/PGvmH533"
 )
-PLAYLIST_IMG_URL = "https://graph.org/file/c95a687e777b55be1c792.jpg"
-STATS_IMG_URL = "https://telegra.ph/file/edd388a42dd2c499fd868.jpg"
-TELEGRAM_AUDIO_URL = "https://telegra.ph/file/492a3bb2e880d19750b79.jpg"
-TELEGRAM_VIDEO_URL = "https://telegra.ph/file/492a3bb2e880d19750b79.jpg"
-STREAM_IMG_URL = "https://graph.org/file/ff2af8d4d10afa1baf49e.jpg"
-SOUNCLOUD_IMG_URL = "https://graph.org/file/c95a687e777b55be1c792.jpg"
-YOUTUBE_IMG_URL = "https://graph.org/file/e8730fdece86a1166f608.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/0bb6f36796d496b4254ff.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/0bb6f36796d496b4254ff.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/0bb6f36796d496b4254ff.jpg"
+PLAYLIST_IMG_URL = "https://ibb.co/PGvmH533"
+STATS_IMG_URL = "https://ibb.co/PGvmH533"
+TELEGRAM_AUDIO_URL = "https://ibb.co/PGvmH533"
+TELEGRAM_VIDEO_URL = "https://ibb.co/PGvmH533"
+STREAM_IMG_URL = "https://ibb.co/PGvmH533"
+SOUNCLOUD_IMG_URL = "https://ibb.co/PGvmH533"
+YOUTUBE_IMG_URL = "https://ibb.co/PGvmH533"
+SPOTIFY_ARTIST_IMG_URL = "https://ibb.co/PGvmH533"
+SPOTIFY_ALBUM_IMG_URL = "https://ibb.co/PGvmH533"
+SPOTIFY_PLAYLIST_IMG_URL = "https://ibb.co/PGvmH533"
 
 
 
