ae59e23e72ab21975e9da09128ccfc6b9d25fdea
yuva neta <anuragyadavop89@gmail.com>
2026-07-13 09:08:50 +0530

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -69,13 +69,15 @@ async def stream_markup_timer(_, chat_id, played, dur):
             ),
         ],
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),  # style=ButtonStyle.PRIMARY
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),  # style=ButtonStyle.PRIMARY
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),  # style=ButtonStyle.PRIMARY
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),  # style=ButtonStyle.PRIMARY
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),  # style=ButtonStyle.DANGER
+
+            InlineKeyboardButton(text="❚❚ ", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="ᴀᴅᴅ ɪᴛ", url=f"https://t.me/{app.username}?startgroup=true"),
+            InlineKeyboardButton(
+                text="|►►", callback_data=f"ADMIN Skip|{chat_id}"
+            ),
         ],
     ]
+
 
     return buttons
 
