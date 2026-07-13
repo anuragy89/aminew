5be002da2dedff7824fbcc2da5018416197269c9
yuva neta <anuragyadavop89@gmail.com>
2026-07-13 09:26:11 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -11,7 +11,7 @@ def start_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true",  # style=ButtonStyle.PRIMARY
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG"),
         ],
     ]
     return buttons
@@ -28,13 +28,12 @@ def private_panel(_):
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_6"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_5"], url=config.SUPPORT_CHANNEL),
+             InlineKeyboardButton(text=_["S_B_5"], url=config.SUPPORT_CHANNEL),
+             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG"),
         ],
-        [InlineKeyboardButton(text="• sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ •", url=f"https://github.com/xbitcode/music.git")],
     
     ]
     
